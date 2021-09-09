63e7e9b2e5230be582ee946f546d3f71248d631f
Adarsh <adarshbhardwaj27082003@gmail.com>
2021-09-09 20:31:10 +0530

    Routers Added

--- src/App.js
+++ src/App.js
@@ -1,42 +1,59 @@
 import { useState } from 'react';
 import './App.css';
 import Alert from './components/Alert';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
 function App() {
   const [mode, setMode] = useState('light'); //Wheter DArk mode is enabled or not
   const [alert, setAlert] = useState(null);
 
-  const showAlert = (message,type)=>{
+  const showAlert = (message, type) => {
     setAlert({
       msg: message,
       type: type
     })
   }
 
-  const toggleMode = ()=>{
+  const toggleMode = () => {
     if (mode === 'light') {
       setMode('dark')
-      document.body.style.backgroundColor='#212529';
-      showAlert("Dark mode has been enabled","success");
+      document.body.style.backgroundColor = '#212529';
+      showAlert("Dark mode has been enabled", "success");
     }
-    else{
+    else {
       setMode('light')
-      document.body.style.backgroundColor='white';
-      showAlert("Light mode has been enabled","success");
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
     }
   }
 
   return (
     <>
       {/* <Navbar title="TextUtils" aboutText='About TextUtils' /> */}
-      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-      <Alert alert={alert}/>
-      <div className="container my-3">
-        <TextForm heading="Enter the text to Analyze" mode={mode}/>
-        {/* <About /> */}
-      </div>
+      <Router>
+        <Navbar title="TextUtils" aboutText='About' mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Switch>
+            <Route exact path="/about">
+              <About />
+            </Route>
+            <Route exact path="/">
+              <TextForm heading="Enter the text to Analyze" mode={mode} />
+            </Route>
+          </Switch>
+          <About />
+        </div>
+      </Router>
     </>
   );
 }
